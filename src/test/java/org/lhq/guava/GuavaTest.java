6c35198fcfca795dd209184a55252aae5f885cb5
Hades <lhqblue@outlook.com>
2021-12-30 18:01:56 +0800

    微小的贡献

---
+++ src/test/java/org/lhq/guava/GuavaTest.java
@@ -0,0 +1,19 @@
+package org.lhq.guava;
+
+import com.google.common.base.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.lhq.entity.User;
+
+@Slf4j
+class GuavaTest {
+	@Test
+	void optional() {
+		User user = new User();
+		user.setUsername("你爸爸");
+		User or = Optional.fromNullable(user).or(new User());
+		log.info("不为空的情况下{}",or);
+		User sdada = Optional.<User>fromNullable(null).or(new User().setNickname("sdada"));
+		log.info("为空的情况下{}",sdada);
+	}
+}
