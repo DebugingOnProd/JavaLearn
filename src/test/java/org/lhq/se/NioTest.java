cced7fc3ea4b6cb2c629bda6cb47717326521ec0
ForWhat <lhqblue@outlook.com>
2022-04-18 14:11:56 +0800

    NIO补充

--- src/test/java/org/lhq/se/NioTest.java
+++ src/test/java/org/lhq/se/NioTest.java
@@ -12,6 +12,7 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.Pipe;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
@@ -19,6 +20,19 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * NIO编程步骤
+ * 1.创建Selector选择器
+ * 2.创建ServerSocketChannel,并绑定监听端口
+ * 3.设置通道的非阻塞模式
+ * 4.吧Channel注册到Selector上
+ * 5.循环调用Selector的select方法,监听通道的就绪情况
+ * 6.获取就绪的Channel集合(selectKeys)
+ * 7.遍历Channel集合判断就绪类型，实现具体业务
+ * 8.根据业务，决定是否再次注册监听事件。重复执行第三部操作
+ */
 
 
 @DisplayName("Nio测试")
@@ -73,6 +87,7 @@ public class NioTest {
 	}
 
 	@Test
+	@DisplayName("选择器/多路复用器")
 	void selector() throws IOException {
 		Selector selector = Selector.open();
 		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
@@ -131,5 +146,56 @@ public class NioTest {
 		//客户端通道连接远程主机端口和IP
 		SocketChannel socketChannel1 = SocketChannel.open(new InetSocketAddress("127.0.0.1",8088));
 
+	}
+
+	@Test
+	@DisplayName("测试管线")
+	void pipe() throws IOException {
+		Pipe pipe = Pipe.open();
+		// 发送数据
+		Pipe.SinkChannel sink = pipe.sink();
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		buffer.put("NIO".getBytes());
+		buffer.flip();
+		//向管线写入数据
+		sink.write(buffer);
+
+		//接受数据
+		Pipe.SourceChannel source = pipe.source();
+		ByteBuffer sourceBuffer = ByteBuffer.allocate(1024);
+		int read = source.read(sourceBuffer);
+		log.info("输出管道消息:{}",new String(sourceBuffer.array(),0,read));
+		source.close();
+		sink.close();
+	}
+
+	//Scatter/Gather
+	@Test
+	@DisplayName("分散于聚集")
+	void scatterAndGathaer(){
+		ByteBuffer header = ByteBuffer.allocate(1024);
+		ByteBuffer body = ByteBuffer.allocate(1024);
+		ByteBuffer[] byteBuffers = {header,body};
+	}
+	@Test
+	@DisplayName("socket监听器")
+	void socketChannel() throws IOException, InterruptedException {
+		ByteBuffer buffer = ByteBuffer.wrap("你好,也可以不好，无所谓".getBytes());
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+		serverSocketChannel.socket().bind(new InetSocketAddress(8888));
+		serverSocketChannel.configureBlocking(false);
+		while (true) {
+			log.info("等待连接");
+			SocketChannel socketChannel = serverSocketChannel.accept();
+			if (socketChannel==null) {
+				log.info("socketChannel为空");
+				TimeUnit.SECONDS.sleep(1);
+			}else {
+				log.info("链接来自:{}",socketChannel.socket().getRemoteSocketAddress());
+				buffer.rewind();
+				socketChannel.write(buffer);
+				socketChannel.close();
+			}
+		}
 	}
 }
