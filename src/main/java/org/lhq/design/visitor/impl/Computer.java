5be7b5718a069563ff712b0a4843ce9344f923aa
DebuggingOnProd <lhqblue@outlook.com>
2022-08-19 12:04:05 +0800

    oh-shit

--- src/main/java/org/lhq/design/visitor/impl/Computer.java
+++ src/main/java/org/lhq/design/visitor/impl/Computer.java
@@ -3,9 +3,21 @@ package org.lhq.design.visitor.impl;
 import org.lhq.design.visitor.ComputerPart;
 import org.lhq.design.visitor.ComputerPartVisitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer implements ComputerPart {
+    private final List<ComputerPart> computerPartList;
+
+    public Computer(){
+        computerPartList = new ArrayList<>();
+        computerPartList.add(new Keyboard());
+        computerPartList.add(new Monitor());
+    }
+
     @Override
     public void accept(ComputerPartVisitor computerPartVisitor) {
+        computerPartList.forEach(item -> item.accept(computerPartVisitor));
         computerPartVisitor.visitor(this);
     }
 }
